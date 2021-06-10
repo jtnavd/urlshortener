34ee1ee8f9dd9aef9d9d8e436e659e10fbae1e3c
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-06-10 11:16:24 +0300

    sdg

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 import dj_database_url
 from pathlib import Path
-
+from settings import PROJECT_ROOT
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
